612b67c0577159c31bc30fb6849287f23e910aae
Thund3rFN <46546779+Thund3rFN@users.noreply.github.com>
2019-01-10 13:05:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ client.on('message', message => {
     }
 });
 
-client.login('NTMyMzA2Njk0MjIzNjkxNzc2.DxgpJg.i-N5Drf7rhbo3GioeVyCdWExCyU')
+client.login('NTMyMzA2Njk0MjIzNjkxNzc2.DxlE2A.htrphxxaHe33mAKVtW6C8DK-R7Q')
